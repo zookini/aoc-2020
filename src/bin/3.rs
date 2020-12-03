daf8ad6de840b669af9e2ada1644d8cf61aaf38f
Prefectly Cromulent <prefectly.cromulent@gmail.com>
2020-12-03 11:04:31 +0000

    Day 3

---
+++ src/bin/3.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Part 1: {}", travel(3, 1));
+
+    println!("Part 2: {}", [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|&(right, down)| travel(right, down))
+        .product::<usize>());
+}
+
+fn travel(right: usize, down: usize) -> usize {
+    include_str!("../../input/3.txt")
+        .lines()
+        .step_by(down)
+        .enumerate()
+        .skip(1)
+        .filter(|(i, row)| row.as_bytes()[i * right % row.len()] == b'#')
+        .count()
+}
