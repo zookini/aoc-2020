400d422ea0eb521dc95491b0737e27d338d0e305
Prefectly Cromulent <prefectly.cromulent@gmail.com>
2020-12-06 14:07:01 +0000

    Day 6 using bitwise operators instead of HashSet

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -1,20 +1,19 @@
 use itertools::Itertools;
-use std::collections::HashSet;
 
 fn main() {
-    println!("Part 1: {}", aggregate(|a, b| &a | &b));
-    println!("Part 2: {}", aggregate(|a, b| &a & &b));
+    println!("Part 1: {}", aggregate(|a, b| a | b));
+    println!("Part 2: {}", aggregate(|a, b| a & b));
 }
 
-fn aggregate(op: fn(HashSet<char>, HashSet<char>) -> HashSet<char>) -> usize {
+fn aggregate(op: fn(u32, u32) -> u32) -> u32 {
     include_str!("../../input/6.txt")
         .split("\r\n\r\n")
         .map(|group| group
             .lines()
-            .map(|line| line.chars().collect())
+            .map(|line| line.bytes().fold(0, |set, b| set | 1 << (b - b'a') as u32))
             .fold1(|a, b| op(a, b))
             .unwrap()
-            .len()
+            .count_ones()
         )
         .sum()
 }
