ccbd94e84d322a69431634ef4dd5d8a11ed99023
Prefectly Cromulent <prefectly.cromulent@gmail.com>
2020-12-06 19:32:11 +0000

    Remove itertools dependency from day 6

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -1,18 +1,15 @@
-use itertools::Itertools;
-
 fn main() {
-    println!("Part 1: {}", aggregate(|a, b| a | b));
-    println!("Part 2: {}", aggregate(|a, b| a & b));
+    println!("Part 1: {}", aggregate(u32::MIN, |a, b| a | b));
+    println!("Part 2: {}", aggregate(u32::MAX, |a, b| a & b));
 }
 
-fn aggregate(op: fn(u32, u32) -> u32) -> u32 {
+fn aggregate(init: u32, op: fn(u32, u32) -> u32) -> u32 {
     include_str!("../../input/6.txt")
         .split("\r\n\r\n")
         .map(|group| group
             .lines()
             .map(|line| line.bytes().fold(0, |set, b| set | 1 << (b - b'a') as u32))
-            .fold1(|a, b| op(a, b))
-            .unwrap()
+            .fold(init, |a, b| op(a, b))
             .count_ones()
         )
         .sum()
