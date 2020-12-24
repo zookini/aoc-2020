3230d3a997ad01eb9dc6d7558f03bd632b308f94
Prefectly Cromulent <prefectly.cromulent@gmail.com>
2020-12-24 17:14:12 +0000

    Optimise Day 24

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,59 +1,58 @@
-use std::collections::HashMap;
+use itertools::iproduct;
+
+type HashMap<K, V> = std::collections::HashMap<K, V, std::hash::BuildHasherDefault<fnv::FnvHasher>>;
+type HashSet<K> = std::collections::HashSet<K, std::hash::BuildHasherDefault<fnv::FnvHasher>>;
 
 fn main() {
-    let tiles = include_str!("../../input/24.txt")
+    let blacks: HashSet<(i32, i32)> = include_str!("../../input/24.txt")
         .lines()
         .map(|line| parse(line.chars()).fold((0, 0), |(x, y), (dx, dy)| (x + dx, y + dy)))
-        .fold(HashMap::new(), |mut tiles, point| {
+        .fold(HashMap::default(), |mut tiles, point| {
             tiles.entry(point).and_modify(|p| *p = if *p == "black" { "white" } else { "black" }).or_insert("black");
             tiles
-        });
+        })
+        .into_iter()
+        .filter(|&(_, colour)| colour == "black")
+        .map(|(point, _)| point)
+        .collect();
     
-    println!("Part 1: {}", run(tiles.clone(), 0));
-    println!("Part 2: {}", run(tiles, 100));
+    println!("Part 1: {}", run(blacks.clone(), 0));
+    println!("Part 2: {}", run(blacks, 100));
 }
 
 fn parse(mut steps: impl Iterator<Item = char>) -> impl Iterator<Item = (i32, i32)> {
     std::iter::from_fn(move || match steps.next() {
         Some('e') => Some((-2, 0)),
         Some('w') => Some((2, 0)),
-        Some('n') => match steps.next() {
-            Some('e') => Some((-1, -1)),
-            Some('w') => Some((1, -1)),
+        Some('n') => Some(match steps.next() {
+            Some('e') => (-1, -1),
+            Some('w') => (1, -1),
             _ => unreachable!()
-        }
-        Some('s') => match steps.next() {
-            Some('e') => Some((-1, 1)),
-            Some('w') => Some((1, 1)),
+        }),
+        Some('s') => Some(match steps.next() {
+            Some('e') => (-1, 1),
+            Some('w') => (1, 1),
             _ => unreachable!()
-        },
+        }),
         _ => None
     })
 }
 
-fn run(tiles: HashMap<(i32, i32), &str>, days: usize) -> usize {
-    std::iter::successors(Some(tiles), |tiles| Some(day(&tiles)))
-        .nth(days)
-        .unwrap()
-        .values()
-        .filter(|&&tile| tile == "black")
-        .count()
+fn run(blacks: HashSet<(i32, i32)>, days: usize) -> usize {
+    (0..days).fold(blacks, |blacks, _| day(&blacks)).len()
 }
 
-fn day<'a>(tiles: &HashMap<(i32, i32), &'a str>) -> HashMap<(i32, i32), &'a str> {
-    tiles
-        .iter()
-        .filter(|(_, &colour)| colour == "black")
-        .flat_map(|((x, y), _)| [(-2, 0), (2, 0), (-1, -1), (1, -1), (-1, 1), (1, 1)].iter().map(move |(dx, dy)| (x + dx, y + dy)))
-        .fold(HashMap::new(), |mut next, point| {
-            next.entry(point).and_modify(|blacks| *blacks += 1).or_insert(1);
+fn day(blacks: &HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
+    iproduct!(blacks, &[(-2, 0), (2, 0), (-1, -1), (1, -1), (-1, 1), (1, 1)])
+        .map(|((x, y), (dx, dy))| (x + dx, y + dy))
+        .fold(HashMap::default(), |mut next, point| {
+            next.entry(point).and_modify(|count| *count += 1).or_insert(1);
             next
         })
         .iter()
-        .map(|(&point, &blacks)| (point, match (*tiles.get(&point).unwrap_or(&"white"), blacks) {
-            ("white", 2) => "black",
-            ("black", n) if n == 0 || n > 2 => "white",
-            (colour, _) => colour,
-        }))
+        .filter_map(|(point, &count)| match (blacks.contains(point), count) {
+            (true, 1) | (true, 2) | (false, 2) => Some(*point),
+            _ => None
+        })
         .collect()
 }
